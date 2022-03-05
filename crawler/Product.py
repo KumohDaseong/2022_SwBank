16e52685105205ec894907c5245be994b220cd24
Jeon_minGyu <coby4313@gmail.com>
2022-03-05 15:04:46 +0900

    url 긁어옴

---
+++ crawler/Product.py
@@ -0,0 +1,16 @@
+class Product():
+    def __init__(self, product_url):
+        self.url = product_url
+        self.img_scr
+        #self.settingProduct()
+    
+    def settingProduct(self):
+        self.name
+        self.price
+        self.review_rating
+        self.standard
+        self.item_model_number
+        self.date_First_Available
+        self.manufacturer
+        self.asin
+        self.review = []
--- crawler/main.py
+++ crawler/main.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
 url_id = input("input url_id : ")
+
 url = "https://www.amazon.com/s?k={0}&crid=9V9C63OT50BV&sprefix={1}%2Caps%2C269&ref=nb_sb_noss_2".format(url_id, url_id)
 
-driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Chrome(options=options)
 driver.get(url)
 
-list = []
+time.sleep(3)
+
+product_list = driver.find_elements_by_class_name("a-link-normal")
+url_list = []
+
+for temp_url in product_list:
+    url_list.append(temp_url.get_attribute("href"))
+
+print(len(product_list))
 
 
